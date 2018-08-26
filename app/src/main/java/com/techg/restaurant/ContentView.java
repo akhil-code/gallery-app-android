8e24f3f9c64b945695f9df9981a340fa1ebf816f
akhil guttula <guttula.akhil@gmail.com>
2018-08-26 19:19:08 +0530

    UI additions

--- app/src/main/java/com/techg/restaurant/ContentView.java
+++ app/src/main/java/com/techg/restaurant/ContentView.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 
@@ -23,7 +25,12 @@ public class ContentView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_content_view);
+
 
         // database classes
         MenuDbHelper menuDbHelper = new MenuDbHelper(getApplicationContext());
--- app/src/main/java/com/techg/restaurant/MainActivity.java
+++ app/src/main/java/com/techg/restaurant/MainActivity.java
@@ -10,9 +10,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -26,7 +29,13 @@ public class MainActivity extends AppCompatActivity implements  AddTagsFragment.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        // hiding action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         // adding data to Database
         addContent();
